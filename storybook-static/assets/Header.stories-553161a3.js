63a73096eb3d618df4938f0c37a2dd906edbccb2
Eddy Quelca <eqteddy@gmail.com>
2023-11-19 17:43:05 -0400

    inicio

--- storybook-static/assets/Header.stories-553161a3.js
+++
@@ -1,8 +0,0 @@
-import{H as p}from"./Header-d4fe8357.js";import"./jsx-runtime-9dc53467.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";import"./Button-f09b49f1.js";/* empty css               */const L={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var t,m,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(m=r.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const f=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,f as __namedExportsOrder,L as default};
-//# sourceMappingURL=Header.stories-553161a3.js.map
