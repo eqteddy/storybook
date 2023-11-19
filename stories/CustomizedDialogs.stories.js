b997f4ff713f3ff22ce57412eeecfe3843b8ffa1
Eddy Quelca <eqteddy@gmail.com>
2023-11-19 15:19:03 -0400

    inicio

---
+++ stories/CustomizedDialogs.stories.js
@@ -0,0 +1,13 @@
+import { CustomizedDialogs } from './CustomizedDialogs'
+export default {
+    title: 'Dialog',
+    component: CustomizedDialogs,
+    
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+}; 
